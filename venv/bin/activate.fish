87f8adcc79fcf67502247c5a2f43b20c1057c9ca
Saravanesh-Git <saravanesh962006@gmail.com>
2025-05-17 10:44:42 +0000

    After Disaster

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /var/www/web/testing/venv
+set -gx VIRTUAL_ENV /var/www/web/Adminer/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
